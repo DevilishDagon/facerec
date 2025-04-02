7255cf3b6057926e38e09ce9076d7e3764c86449
DevilishDagon <152270339+DevilishDagon@users.noreply.github.com>
2025-04-02 17:41:21 +0200

    Update camera_module.py

--- camera_module.py
+++ camera_module.py
@@ -14,7 +14,7 @@ class CameraManager:
                 self.picam2 = Picamera2()
                 time.sleep(1)  # Let system settle
 
-                config = self.picam2.create_preview_configuration(main={"size": (width, height)})
+                config = self.picam2.create_preview_configuration(main={"size": (1280, 720)})
                 self.picam2.configure(config)
                 self.picam2.start(show_preview=False)
                 self.picam2.set_controls({"AfMode": controls.AfModeEnum.Continuous})
